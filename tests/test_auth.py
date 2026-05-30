d8b094447d6557e209d5486fe09c515170cba6f3
raihan2030 <raihan.muhammad.bjm@gmail.com>
2026-05-31 00:15:15 +0800

    Refactor TestAuth: reorder test cases for better clarity and maintainability

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -22,21 +22,21 @@ class TestAuth(unittest.TestCase):
         self.login_page.login_as("Admin", "admin123")
         self.assertEqual(self.dashboard_page.get_dashboard_header(), "Dashboard")
 
-    # --- NEGATIVE TEST CASE ---
-    
-    def test_02_login_invalid_password(self):
-        self.login_page.login_as("Admin", "salahpassword")
-        self.assertEqual(self.login_page.get_error_message(), "Invalid credentials")
-
-    def test_03_login_empty_username(self):
-        self.login_page.enter_password("admin123")
-        self.login_page.click_login()
-        self.assertTrue(self.login_page.is_required_message_displayed())
-
-    def test_04_logout_berhasil(self):
+    def test_02_logout_berhasil(self):
         self.login_page.login_as("Admin", "admin123")
         self.dashboard_page.logout()
         self.assertTrue(self.login_page.is_login_button_displayed())
+
+    # --- NEGATIVE TEST CASE ---
+    
+    def test_03_login_invalid_password(self):
+        self.login_page.login_as("Admin", "salahpassword")
+        self.assertEqual(self.login_page.get_error_message(), "Invalid credentials")
+
+    def test_04_login_empty_username(self):
+        self.login_page.enter_password("admin123")
+        self.login_page.click_login()
+        self.assertTrue(self.login_page.is_required_message_displayed())
 
     def test_05_login_empty_password(self):
         self.login_page.enter_username("Admin")
